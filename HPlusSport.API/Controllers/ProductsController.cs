20a4b4552e175b7f7cc47b02fc9673e3ce1240f8
razen <razen@DESKTOP-92M0EQ0>
2025-03-15 21:18:25 +0900

    Complete crud operations using attribute/controller routing and minimal apis.

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -37,13 +37,98 @@ namespace HPlusSport.API.Controllers
         {
             var product = await _context.Products.FindAsync(id);
 
-            if (product == null) { 
+            if (product == null)
+            {
                 return NotFound();
             }
 
             return Ok(product);
         }
 
+        [HttpGet("available")]
+        public async Task<ActionResult> GetAvailableProducts()
+        {
+            var availableProducts = await _context.Products.Where(x => x.IsAvailable == true).ToArrayAsync();
+            if (availableProducts == null)
+            {
+                return NotFound();
+            }
+            return Ok(availableProducts);
+        }
 
+        [HttpPost]
+        public async Task<ActionResult> PostProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                "GetProduct",
+                new { id = product.Id },
+                product
+                );
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Products.Any(x => x.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if(product == null) return NotFound();
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        [HttpPost("Delete")]
+        public async Task<ActionResult> DeleteMultiple([FromQuery]int[] ids)
+        {
+            var products = new List<Product>();
+            foreach(var id in ids)
+            {
+                var product = await _context.Products.FindAsync(id);
+                if (product == null) return NotFound();
+                products.Add(product);
+            }
+
+            _context.Products.RemoveRange(products);
+            await _context.SaveChangesAsync();
+
+            return Ok(products);
+        }
     }
 }
--- HPlusSport.API/Program.cs
+++ HPlusSport.API/Program.cs
@@ -1,4 +1,5 @@
 using HPlusSport.API.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,11 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        // This prevents sending error message back to the caller
+        //options.SuppressModelStateInvalidFilter = true;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ShopContext>(options => {
+builder.Services.AddDbContext<ShopContext>(options =>
+{
     options.UseInMemoryDatabase("Shop");
 });
 
@@ -28,5 +37,105 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ShopContext>();
+    await db.Database.EnsureCreatedAsync();
+}
+
+app.MapGet("/products", async (ShopContext _context) =>
+{
+    return await _context.Products.ToArrayAsync();
+});
+
+app.MapGet("/products/{id}", async (int id, ShopContext _context) =>
+{
+    var product = await _context.Products.FindAsync(id);
+
+
+    if (product == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(product);
+});
+
+app.MapGet("/products/available", async (ShopContext _context) =>
+{
+    var availableProducts = await _context.Products.Where(x => x.IsAvailable == true).ToArrayAsync();
+    if (availableProducts == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(availableProducts);
+}).WithName("GetProduct");
+
+app.MapDelete("/products/{id}", async (int id, ShopContext _context) =>
+{
+    var product = await _context.Products.FindAsync(id);
+    if (product == null) return Results.NotFound();
+
+    _context.Products.Remove(product);
+    await _context.SaveChangesAsync();
+
+    return Results.Ok(product);
+});
+
+app.MapPut("/products/{id}", async (int id, Product product, ShopContext _context) =>
+{
+    if (id != product.Id)
+    {
+        return Results.BadRequest();
+    }
+
+    _context.Entry(product).State = EntityState.Modified;
+
+    try
+    {
+        await _context.SaveChangesAsync();
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+        if (!_context.Products.Any(x => x.Id == id))
+        {
+            return Results.NotFound();
+        }
+        else
+        {
+            throw;
+        }
+    }
+    return Results.NoContent();
+});
+
+app.MapPost("/products", async (Product product, ShopContext _context) =>
+{
+    _context.Products.Add(product);
+    await _context.SaveChangesAsync();
+
+    return Results.CreatedAtRoute(
+        "GetProduct",
+        new { id = product.Id },
+        product
+        );
+});
+
+app.MapPost("/products/{ids}", async (int[] ids, ShopContext _context) =>
+{
+    var products = new List<Product>();
+    foreach (var id in ids)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null) return Results.NotFound();
+        products.Add(product);
+    }
+
+    _context.Products.RemoveRange(products);
+    await _context.SaveChangesAsync();
+    
+    return Results.Ok(products);
+});
 
 app.Run();
