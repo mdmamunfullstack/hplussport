eb2f88da584293e2edea3f3d62088a4aa74b4bb2
razen <razen@DESKTOP-92M0EQ0>
2025-03-15 16:39:13 +0900

    Added asynchronous http get methods to retrieve one or more Products

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HPlusSport.API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HPlusSport.API.Controllers
 {
@@ -7,10 +9,41 @@ namespace HPlusSport.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts()
+        private readonly ShopContext _context;
+        public ProductsController(ShopContext shopContext)
         {
-            return "OK";
+            _context = shopContext;
+
+            _context.Database.EnsureCreated();
         }
+
+        /*
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _context.Products.ToArray();
+        }
+        */
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAllProducts()
+        {
+            return Ok(await _context.Products.ToArrayAsync());
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null) { 
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+
     }
 }
